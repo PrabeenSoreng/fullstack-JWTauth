28ba37a0ff864463ded49ecd49063db2866bf916
Prabeen <prabeensoreng2119@gmail.com>
2018-12-23 00:52:51 +0530

    Adding server side validation for signup functionality

--- login-back/controllers/signup.js
+++ login-back/controllers/signup.js
@@ -1,3 +1,33 @@
-exports.createUser = (req, res) => {
+const Joi = require('joi');
 
-}
+const User = require('../models/user');
+
+exports.createUser = async(req, res) => {
+    const Schema = Joi.object().keys({
+        username: Joi.string().min(3).max(20).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(5).required()
+    });
+
+    const { error, value } = Joi.validate(req.body, Schema);
+    if (error && error.details) return res.status(400).json({ message: error.details });
+
+    const username = await User.findOne({ username: req.body.username });
+    if (username) return res.status(409).json({ message: 'Username already exists' });
+
+    const email = await User.findOne({ email: req.body.email });
+    if (email) return res.status(409).json({ message: 'Email already exists' });
+
+
+    User.create({
+            username: value.username,
+            email: value.email,
+            password: value.password
+        })
+        .then(user => {
+            res.status(201).json({ message: 'User created successfully', user });
+        })
+        .catch(err => {
+            res.status(500).json({ message: err });
+        });
+};
--- login-back/models/user.js
+++ login-back/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const Schema = mongoose.Schema;
 
@@ -15,6 +16,13 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre('save', async function(next) {
+    if (this.password && this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
