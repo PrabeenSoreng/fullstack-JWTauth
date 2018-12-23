fb135159472516cb6d231e3236b0b34be5932d78
Prabeen <prabeensoreng2119@gmail.com>
2018-12-23 22:56:17 +0530

    auth guard implemented

--- login-front/src/app/routing/routing.module.ts
+++ login-front/src/app/routing/routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { SignupComponent } from '../signup/signup.component';
 import { HomeComponent } from '../home/home.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: ''}
 ];
 
